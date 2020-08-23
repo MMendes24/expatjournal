265068230303c94825b05678f1fd22c9ff645b54
boahs <39522778+boahs@users.noreply.github.com>
2020-08-23 08:41:49 -0400

    validation working. initial singup form.

--- expatjournal/src/App.js
+++ expatjournal/src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import SignUpForm from "./components/SignUpForm";
+import axios from "axios";
+import * as yup from "yup";
+
+// const dummyData = {
+//   sha: { username: "Sha", age: 29, id: 0 },
+//   tom: { username: "Tom", age: 30, id: 1 },
+// };
+
+const formSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(2, "Username must be at least 2 characters long")
+    .required("Username is Required!"),
+  password: yup
+    .string()
+    .min(5, "Your password must be 5 or more characters")
+    .required("Password is required!"),
+  email: yup
+    .string()
+    .email("Must be a valid email address")
+    .required("Email is required!"),
+});
+
+const initialFormValues = {
+  username: "",
+  password: "",
+  email: "",
+};
+
+const initialFormErrors = {
+  username: "",
+  password: "",
+  email: "",
+};
+
+const initialUsers = [];
+const initialDisabled = true;
 
 function App() {
+  const [user, setUser] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const getUsers = () => {
+    axios
+      .get("https://reqres.in/api/users") //users
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        debugger;
+      });
+  };
+
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        setUser([...user, res.data]);
+      })
+      .catch((err) => {
+        debugger;
+      })
+      .finally(() => {
+        setFormValues(initialFormValues);
+      });
+  };
+
+  const inputChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const submit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      password: formValues.password.trim(),
+      email: formValues.email,
+    };
+    postNewUser(newUser);
+  };
+
+  useEffect(() => {
+    getUsers();
+    console.log(formValues);
+    console.log(user);
+    console.log(initialUsers);
+  }, []);
+  useEffect(() => {
+    formSchema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignUpForm
+        values={formValues}
+        inputChange={inputChange}
+        submit={submit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+      <div className="App"></div>
     </div>
   );
 }
 
 export default App;
+
+// {
+//   Object.values(user).map((x) => (
+//     <SignUp key={x.id} username={x.username} age={x.age} />
+//   ));
+// }
---
+++ expatjournal/src/components/SignUpForm.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 40vh;
+`;
+
+const SignUpForm = (props) => {
+  const { values, submit, inputChange, disabled, errors } = props;
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+  const onInputChange = (evt) => {
+    const { name, value } = evt.target;
+    inputChange(name, value);
+  };
+
+  return (
+    <form className="form container" onSubmit={onSubmit}>
+      <StyledDiv>
+        <h2>Sign up</h2>
+        <div className="errors">
+          <div>{errors.username}</div>
+          <div>{errors.password}</div>
+          <div>{errors.email}</div>
+        </div>
+        <div className="form-group">
+          <label>
+            <input
+              value={values.username}
+              onChange={onInputChange}
+              type="text"
+              name="username"
+              placeholder="Username"
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <div className="form-group">
+            <label>
+              <input
+                value={values.password}
+                onChange={onInputChange}
+                type="text"
+                name="password"
+                placeholder="Password"
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              <input
+                value={values.email}
+                onChange={onInputChange}
+                type="email"
+                name="email"
+                placeholder="email"
+              />
+            </label>
+          </div>
+          <button disabled={disabled}>Sign up</button>
+        </div>
+      </StyledDiv>
+    </form>
+  );
+};
+
+export default SignUpForm;
