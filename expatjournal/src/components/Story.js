4219c7cecff37eee5d33d4601baa959d074be721
MMendes24 <mmbusiness1@protonmail.com>
2020-08-24 15:23:57 -0400

    Story component created. No functionality.

---
+++ expatjournal/src/components/Story.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Story = () => {
+    return (
+        <section>This is a test section. It is only a test section. Avert thine eyes.</section>
+    )
+}
+
+export default Story
