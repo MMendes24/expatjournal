509f1be3ae4a2490f4f46bea2a73b68237c2f7b3
Joe Bay <jbay55278@gmail.com>
2020-08-24 13:25:21 -0800

    added the functionality to create the individual story cards and the button to link to the add story component

--- expatjournal/src/components/Dashboard.js
+++ expatjournal/src/components/Dashboard.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { getStories } from '../actions/getStories'
+import { useHistory } from 'react-router-dom'
 
 const Dashboard = () => {
+    const history = useHistory()
+
+    useEffect(() => {
+        props.getStories()
+    },[])
+
     return (
         <div className='dashboard-container'>
         <h1>Welcome to the Dashboard </h1>
-            <div>
-                <h3>Stories go here</h3>
+        <button onClick={() => history.push('/add-story')}>Add Story</button>
+
+        {props.isLoading ? <h4>Loading your stories.. Please wait a moment.</h4> : null}
+        {props.error ? <h4>There was an error <br/> {props.error}</h4> : null}
+        {props.stories.length > 0 ? (
+            <div className='stories-container'>
+                {props.stories.map(story => (
+                    <div className='story-card'>
+                        <h2>{story.title}</h2>
+                        <h4>{story.location}</h4>
+                        <h6>{story.time}</h6>
+                        <p>{story.story}</p>
+                    </div>
+                ))}
             </div>
+        ): null}
         </div>
     )
 }
