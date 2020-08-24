9c58181cf48f17c4d347e9cc825b8d97d4b69a9f
Joe Bay <jbay55278@gmail.com>
2020-08-24 13:45:04 -0800

    added the cases for the getStories request in the reducer

--- expatjournal/src/reducers/storyReducer.js
+++ expatjournal/src/reducers/storyReducer.js
@@ -1,3 +1,5 @@
+import { FETCH_STORIES_START, FETCH_STORIES_SUCCESS, FETCH_STORIES_ERROR } from '../actions/getStories'
+
 const initialState = {
     isLoading: false,
     stories: [],
@@ -7,6 +9,26 @@ const initialState = {
 
 const storyReducer = ( state = initialState, action ) => {
     switch(action.type){
+        case FETCH_STORIES_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: '',
+            }
+        case FETCH_STORIES_SUCCESS:
+            return {
+                ...state,
+                stories: action.payload,
+                isLoading: false,
+                error: '',
+            }
+        case FETCH_STORIES_ERROR:
+            return {
+                ...state,
+                stories: [],
+                isLoading: false,
+                error: action.payload
+            }
         default: return state
     }
 }
