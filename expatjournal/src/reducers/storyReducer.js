d4610801be59986d5864bfe5a8a52a305c26e4fa
Joe Bay <jbay55278@gmail.com>
2020-08-25 07:48:15 -0800

    added cases to the reducer file for addStory

--- expatjournal/src/reducers/storyReducer.js
+++ expatjournal/src/reducers/storyReducer.js
@@ -1,4 +1,5 @@
 import { FETCH_STORIES_START, FETCH_STORIES_SUCCESS, FETCH_STORIES_ERROR } from '../actions/getStories'
+import { ADD_STORY_START, UPDATE_STORY_LIST, ADD_STORY_ERROR } from '../actions/addStoryAction'
 
 const initialState = {
     isLoading: false,
@@ -26,6 +27,24 @@ const storyReducer = ( state = initialState, action ) => {
             return {
                 ...state,
                 stories: [],
+                isLoading: false,
+                error: action.payload
+            }
+        case ADD_STORY_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: '',
+            }
+        case UPDATE_STORY_LIST:
+            return {
+                ...state,
+                isLoading: false,
+                stories: [action.payload, ...state.stories]
+            }
+        case ADD_STORY_ERROR:
+            return {
+                ...state,
                 isLoading: false,
                 error: action.payload
             }
