2010ee0be3aae78d5de18a7b00057d23444809c3
Joe Bay <jbay55278@gmail.com>
2020-08-27 08:44:41 -0800

    added the nav bar and nav bar styling

---
+++ expatjournal/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const StyledNav = styled.nav`
+    display:flex;
+    justify-content: space-around;
+    margin: 4%;
+    padding: 1%;
+    font-size: 1.8rem;
+    text-decoration:none;
+    h1{
+        font-size: 2.4rem;
+        text-align:left;
+        margin: 0;
+    }
+    a{
+        text-decoration: none;
+        font-family: 'Patrick Hand', cursive;
+        color:black;
+    }
+    a:hover{
+        font-size: 2.2rem;
+    }
+    `
+
+
+    const id = localStorage.getItem('user_id')
+    const history = useHistory()
+    return (
+        <StyledNav className='nav-bar'>
+            <h1>The World Through My Eyes</h1>
+            <a href='https://wizardly-mclean-829421.netlify.app/index.html'>Home</a>
+            <Link to='/'>Login</Link>
+            <Link to={`/register`}>Register</Link>
+            <Link to={`/dashboard/${id}`}>Dashboard</Link>
+        </StyledNav>
+    )
+}
+
+export default Navbar
